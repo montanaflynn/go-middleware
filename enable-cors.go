d3e5b66baefea6062a3e395fb93a73d72241797b
Montana Flynn <montana@montanaflynn.me>
2014-11-15 22:14:31 -0800

    Clean up indentation

--- enable-cors.go
+++ enable-cors.go
@@ -1,26 +1,26 @@
 package main
  
 import (
-    "net/http"
+	"net/http"
 )
  
 func enableCORS(handler http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(200)
-        }
-        handler.ServeHTTP(w, r)
-    })
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(200)
+		}
+		handler.ServeHTTP(w, r)
+	})
 }
  
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello world!"))
+	w.Write([]byte("Hello world!"))
 }
  
 func main() {
-    app := enableCORS(http.HandlerFunc(helloWorld)) 
-    http.ListenAndServe(":8080", app)
+	app := enableCORS(http.HandlerFunc(helloWorld)) 
+	http.ListenAndServe(":8080", app)
 }
