0e74f3d44c6c0f3cf7556af26aa16f3f55a38542
Montana Flynn <montana@montanaflynn.me>
2014-11-15 22:16:09 -0800

    Clean up indentation

--- delay.go
+++ delay.go
@@ -2,27 +2,27 @@ package main
  
 import (
 	"log"
-    "time"
-    "net/http"
+	"time"
+	"net/http"
 )
  
 func delay(handler http.Handler, length string) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    	timeout, err := time.ParseDuration(length)
-    	if err != nil {
-    		log.Println("Could not parse delay length, skipping delay.")
-    		handler.ServeHTTP(w, r)
-    	}
-       	time.Sleep(timeout)
-       	handler.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		timeout, err := time.ParseDuration(length)
+		if err != nil {
+			log.Println("Could not parse delay length, skipping delay.")
+			handler.ServeHTTP(w, r)
+		}
+		time.Sleep(timeout)
+		handler.ServeHTTP(w, r)
+	})
 }
  
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello world!"))
+	w.Write([]byte("Hello world!"))
 }
  
 func main() {
-    app := delay(http.HandlerFunc(helloWorld), "5s") 
-    http.ListenAndServe(":8080", app)
+	app := delay(http.HandlerFunc(helloWorld), "5s") 
+	http.ListenAndServe(":8080", app)
 }
